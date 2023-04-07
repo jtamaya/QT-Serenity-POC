ea856c195625b2bef84c56aa9b54b4122838ec51
Amaya <jonathan.amaya@softwareone.com>
2023-04-07 09:34:12 -0400

    fix for not clickable

--- src/test/java/UIPages/DashboardPage.java
+++ src/test/java/UIPages/DashboardPage.java
@@ -4,6 +4,6 @@ import net.thucydides.core.pages.PageObject;
 
 public class DashboardPage extends PageObject {
     public void clickAutomateNavButton(){
-        $("//*[@id=\"header\"]/header/div/div/div/nav/ul/li[3]").click();
+        $("/html/body/div[1]/header/div/div/div/nav/ul/li[3]/a").click();
     }
 }
